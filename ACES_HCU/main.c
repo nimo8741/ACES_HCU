a52aa507960696d61b3eb163c7852dd3135cefb5
nimo8741 <nimo8741@colorado.edu>
2018-01-16 23:05:14 -0700

    Initial Commit
    Created instance of the repository

---
+++ ACES_HCU/main.c
@@ -0,0 +1,18 @@
+/*
+ * ACES_HCU.c
+ *
+ * Created: 1/16/2018 10:35:41 PM
+ * Author : Nick
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
